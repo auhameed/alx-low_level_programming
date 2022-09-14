90bfee74803e80dcb40619a2125f7b61d394d5ae
auhameed <uabdulhameed1@gmail.com>
2022-09-14 08:24:32 -0700

    added 2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - prints 10 times the alphabet
+*
+*/
+
+void print_alphabet_x10(void)
+{
+	char letter;
+	int num;
+
+	for (num = 1; num < 10; num++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+}
